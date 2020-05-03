0f8a9d85489df5e2d8fc34dcfbdfe52818ac59c1
azhwani <azhwani@devwithus.com>
2020-05-03 16:17:33 +0100

    first commit

---
+++ src/test/java/com/restapi/msusers/MsusersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.msusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsusersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
